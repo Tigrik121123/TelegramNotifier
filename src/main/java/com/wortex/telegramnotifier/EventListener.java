e49bc45009712e5d664cb5885efbc8ef256a8859
Tigrik121123 <wortexcorporation@gmail.com>
2025-05-01 03:28:12 +0500

    Update EventListener.java

--- src/main/java/com/wortex/telegramnotifier/EventListener.java
+++ src/main/java/com/wortex/telegramnotifier/EventListener.java
@@ -1,4 +1,4 @@
-﻿package com.wortex.telegramnotifier; // Замени на свой пакет
+package com.wortex.telegramnotifier; // Замени на свой пакет
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;

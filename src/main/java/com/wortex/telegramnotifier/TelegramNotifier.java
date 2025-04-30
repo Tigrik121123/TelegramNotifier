c607b47503e0a2d03e5776ef6f1c6e989bc80dff
Tigrik121123 <wortexcorporation@gmail.com>
2025-05-01 03:29:42 +0500

    Update TelegramNotifier.java

--- src/main/java/com/wortex/telegramnotifier/TelegramNotifier.java
+++ src/main/java/com/wortex/telegramnotifier/TelegramNotifier.java
@@ -1,4 +1,4 @@
-﻿package com.wortex.telegramnotifier; // Замени на свой пакет
+package com.wortex.telegramnotifier; // Замени на свой пакет
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;

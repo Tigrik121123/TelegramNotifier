5da39c2e1873af83cca9a9228b02d1ba81b4a4ac
Tigrik121123 <wortexcorporation@gmail.com>
2025-05-01 03:29:22 +0500

    Update SetCredentialsCommand.java

--- src/main/java/com/wortex/telegramnotifier/SetCredentialsCommand.java
+++ src/main/java/com/wortex/telegramnotifier/SetCredentialsCommand.java
@@ -1,4 +1,4 @@
-﻿package com.wortex.telegramnotifier; // Замени на свой пакет
+package com.wortex.telegramnotifier; // Замени на свой пакет
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
